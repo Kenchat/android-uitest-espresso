8ab7203901ec3e4d3e604db7b063fb5d742ae4d7
Ngọc Hạnh Võ <nghiamy15@gmail.com>
2020-05-21 16:18:07 +0700

    fixed mock apis

--- app/src/androidTest/java/com/kbtg/android/espresso/countrydetail/dispatcher/CountryDetailActivityDispatcher.kt
+++ app/src/androidTest/java/com/kbtg/android/espresso/countrydetail/dispatcher/CountryDetailActivityDispatcher.kt
@@ -1,8 +1,9 @@
 package com.kbtg.android.espresso.countrydetail.dispatcher
 
+import android.util.Log
 import com.kbtg.android.espresso.base.DispatcherUtil
 import com.kbtg.android.espresso.base.PathJsonConstants
-import com.kbtg.android.espresso.network.ApiConstants.API_DAYONE_COUNTRY
+import com.kbtg.android.espresso.network.ApiConstants.API_DAYONE_COUNTRY_THAI
 import com.kbtg.android.espresso.network.ApiConstants.API_SUMMARY
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
@@ -11,15 +12,20 @@ import okhttp3.mockwebserver.RecordedRequest
 class CountryDetailActivityDispatcher : Dispatcher() {
 
     override fun dispatch(request: RecordedRequest): MockResponse {
+        Log.d("LOG", "CountryDetailActivityDispatcher | url: ${request.requestUrl} | path: ${request.path}")
         return when (request.path) {
             API_SUMMARY -> {
                 val mock = DispatcherUtil.getMockResponse(PathJsonConstants.API_SUMMARY)
+                Log.d("LOG", "\t\t\t\t | ${PathJsonConstants.API_SUMMARY} | mock: ${mock}")
                 mock
             }
-            API_DAYONE_COUNTRY -> {
-                DispatcherUtil.getMockResponse(PathJsonConstants.API_DAYONE_COUNTRY)
+            API_DAYONE_COUNTRY_THAI -> {
+                val mock = DispatcherUtil.getMockResponse(PathJsonConstants.API_DAYONE_COUNTRY)
+                Log.d("LOG", "\t\t\t\t | ${PathJsonConstants.API_DAYONE_COUNTRY} | mock: ${mock}")
+                mock
             }
             else -> {
+                Log.d("LOG", "\t\t\t\t | empty response")
                 DispatcherUtil.getEmptyResponse()
             }
         }
--- app/src/main/java/com/kbtg/android/espresso/network/ApiConstants.kt
+++ app/src/main/java/com/kbtg/android/espresso/network/ApiConstants.kt
@@ -2,6 +2,7 @@ package com.kbtg.android.espresso.network
 
 object ApiConstants {
 
-    const val API_SUMMARY = "summary"
-    const val API_DAYONE_COUNTRY = "dayone/country/{country}"
+    const val API_SUMMARY = "/summary"
+    const val API_DAYONE_COUNTRY = "/dayone/country/{country}"
+    const val API_DAYONE_COUNTRY_THAI = "/dayone/country/Thailand"
 }
