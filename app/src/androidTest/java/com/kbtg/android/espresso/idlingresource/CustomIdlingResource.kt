b44570ab0cfbca8ecc67f06d2d1e2b02b778e7d9
Duy <>
2020-05-27 16:15:02 +0700

    add comment idling resource

--- app/src/androidTest/java/com/kbtg/android/espresso/idlingresource/CustomIdlingResource.kt
+++ app/src/androidTest/java/com/kbtg/android/espresso/idlingresource/CustomIdlingResource.kt
@@ -2,12 +2,20 @@ package com.kbtg.android.espresso.idlingresource
 
 import androidx.test.espresso.IdlingResource
 
+/**
+ * An idling resource represents an asynchronous operation whose results
+ * affect subsequent operations in a UI test.
+ *
+ * Idling Resources synchronize Espresso tests with your app’s background tasks.
+ *
+ * https://android.jlelse.eu/integrate-espresso-idling-resources-in-your-app-to-build-flexible-ui-tests-c779e24f5057
+ */
 class CustomIdlingResource : IdlingResource {
     private var mIsIdle = false
     private var resourceCallback: IdlingResource.ResourceCallback? = null
 
     fun updateIdleState() {
-        mIsIdle = true //false = espresso is pending and cannot do actions
+        mIsIdle = true //false = app is busy, espresso pause actions
         resourceCallback?.onTransitionToIdle()
     }
 
