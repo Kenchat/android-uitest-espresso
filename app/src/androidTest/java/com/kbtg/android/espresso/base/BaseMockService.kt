377534182b650a7e06f6ba36a980fa78d3b5e65a
Duy <>
2020-05-27 13:53:42 +0700

    remove grant permission

--- app/src/androidTest/java/com/kbtg/android/espresso/base/BaseMockService.kt
+++ app/src/androidTest/java/com/kbtg/android/espresso/base/BaseMockService.kt
@@ -3,7 +3,6 @@ package com.kbtg.android.espresso.base
 import android.content.Intent
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import androidx.test.rule.GrantPermissionRule
 import com.kbtg.android.espresso.idlingresource.CustomIdlingResTestRule
 import com.kbtg.android.espresso.ui.nationlist.view.NationListActivity
 import okhttp3.mockwebserver.Dispatcher
@@ -24,13 +23,6 @@ abstract class BaseMockService {
     @Rule
     @JvmField
     val idlingResourceTestRule = CustomIdlingResTestRule()
-
-    @Rule
-    @JvmField
-    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        "android.permission.READ_EXTERNAL_STORAGE",
-        "android.permission.WRITE_EXTERNAL_STORAGE"
-    )
 
     @Before
     open fun setup() {
