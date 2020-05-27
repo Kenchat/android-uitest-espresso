e7f9eeb3198597f00b99577c78ff255785af3c48
Sang Truong Hoang <truonghoangsang93@gmail.com>
2020-05-27 10:20:01 +0700

    Remove main and page 2 activity

--- app/src/main/java/com/kbtg/android/espresso/di/module/ActivityBuilder.kt
+++ app/src/main/java/com/kbtg/android/espresso/di/module/ActivityBuilder.kt
@@ -2,15 +2,11 @@ package com.kbtg.android.espresso.di.module
 
 import android.app.Application
 import com.kbtg.android.espresso.ApplicationClass
-import com.kbtg.android.espresso.di.module.main.MainActivityModule
 import com.kbtg.android.espresso.di.module.nationdetail.NationDetailActivityModule
 import com.kbtg.android.espresso.di.module.nations.NationActivityModule
-import com.kbtg.android.espresso.di.module.page2.Page2ActivityModule
 import com.kbtg.android.espresso.di.scope.PerActivity
 import com.kbtg.android.espresso.ui.countrydetail.view.NationDetailActivity
-import com.kbtg.android.espresso.ui.main.view.MainActivity
 import com.kbtg.android.espresso.ui.nationlist.view.NationListActivity
-import com.kbtg.android.espresso.ui.page2.view.Page2Activity
 import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -24,18 +20,10 @@ abstract class ActivityBuilder {
     internal abstract fun application(app: ApplicationClass): Application
 
     @PerActivity
-    @ContributesAndroidInjector(modules = [MainActivityModule::class])
-    internal abstract fun mainActivityInjector(): MainActivity
-
-    @PerActivity
     @ContributesAndroidInjector(modules = [NationActivityModule::class])
     internal abstract fun nationListActivityInjector(): NationListActivity
 
     @PerActivity
     @ContributesAndroidInjector(modules = [NationDetailActivityModule::class])
     internal abstract fun nationDetailActivityInjector(): NationDetailActivity
-
-    @PerActivity
-    @ContributesAndroidInjector(modules = [Page2ActivityModule::class])
-    internal abstract fun page2ActivityInjector(): Page2Activity
 }
--- app/src/main/java/com/kbtg/android/espresso/ui/page2/presenter/IPage2Presenter.kt
+++
@@ -1,3 +0,0 @@
-package com.kbtg.android.espresso.ui.page2.presenter
-
-interface IPage2Presenter
