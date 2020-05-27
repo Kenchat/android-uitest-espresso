ac66a19f9aa808dbef190537e32f0bff07d82fbb
Sang Truong Hoang <truonghoangsang93@gmail.com>
2020-05-27 10:57:29 +0700

    Remove unused code

--- app/src/main/java/com/kbtg/android/espresso/ui/base/BaseActivity.kt
+++ app/src/main/java/com/kbtg/android/espresso/ui/base/BaseActivity.kt
@@ -1,12 +1,9 @@
 package com.kbtg.android.espresso.ui.base
 
-import android.app.ProgressDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import com.kbtg.android.espresso.R
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -14,8 +11,6 @@ import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
 abstract class BaseActivity : AppCompatActivity(), HasAndroidInjector, IBaseView {
-
-    private var mProgressDialog: ProgressDialog? = null
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
@@ -28,71 +23,19 @@ abstract class BaseActivity : AppCompatActivity(), HasAndroidInjector, IBaseView
         super.onCreate(savedInstanceState)
 
         setContentView(setLayout())
-        initializeProgressDialog()
         init(savedInstanceState)
-    }
-
-    private fun initializeProgressDialog() {
-        mProgressDialog = ProgressDialog(this).apply {
-            isIndeterminate = true
-            setCancelable(false)
-        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         System.gc()
         System.runFinalization()
-        dismissProgress()
-        mProgressDialog = null
     }
 
     @LayoutRes
     abstract fun setLayout(): Int
     abstract fun init(savedInstanceState: Bundle?)
     abstract fun stopScreen()
-
-    private fun showProgress(
-        msgResId: Int,
-        keyListener: DialogInterface.OnKeyListener?
-    ) {
-        mProgressDialog?.run {
-            if (isFinishing)
-                return
-
-            if (isShowing) {
-                return
-            }
-
-            if (msgResId != 0) {
-                setMessage(resources.getString(msgResId))
-            }
-
-            if (keyListener != null) {
-                setOnKeyListener(keyListener)
-
-            } else {
-                setCancelable(false)
-            }
-            show()
-        }
-    }
-
-    private fun dismissProgress() {
-        mProgressDialog?.run {
-            if (isShowing) {
-                dismiss()
-            }
-        }
-    }
-
-    override fun hideLoading() {
-        dismissProgress()
-    }
-
-    override fun showLoading() {
-        showProgress(R.string.loading, null)
-    }
 
     override fun loadError(e: Throwable) {
         showHttpError(e)
